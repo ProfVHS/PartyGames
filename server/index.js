6f2505162e4acc9ad88e915c64fe933c32a55752
Worekit <worekit@gmail.com>
2023-09-15 19:49:27 +0200

    first commit :D

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors") 
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app) 
+const io = new Server(server, {
+    cors: {origin: "http://localhost:5173"}
+})
+
+io.on('connection', (socket) => {
+    
+})
+
+server.listen(3000, () => {
+    console.log('serwer cię słyszy')
+});
