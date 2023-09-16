39d8e6738970276c08746cde116295499f0ba36a
Rafal Rubaj <rubajrafal04@gmail.com>
2023-09-16 18:24:20 +0200

    Audio Video Controls, added click button audio in lobby

--- client/src/components/Lobby.tsx
+++ client/src/components/Lobby.tsx
@@ -1,10 +1,15 @@
 import "../styles/Lobby.scss";
 
+import ClickSound from "../assets/audio/click.mp3";
+
 interface LobbyProps {
   roomCode: string;
 }
 
 function Lobby({ roomCode }: LobbyProps) {
+  const handleReadyClick = () => {
+    new Audio(ClickSound).play();
+  };
   return (
     <div className="lobby">
       <span className="lobby__roomcode">Room Code: {roomCode}</span>
@@ -12,7 +17,9 @@ function Lobby({ roomCode }: LobbyProps) {
         <span className="lobby__playersready-count">0</span>
         <span className="lobby__playersready-text">players ready</span>
       </div>
-      <button className="lobby__button">Ready</button>
+      <button className="lobby__button" onClick={handleReadyClick}>
+        Ready
+      </button>
     </div>
   );
 }
