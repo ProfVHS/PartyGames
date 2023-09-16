8288111e13e2f3c51df647b89a7a34be8f8cff69
Rafal Rubaj <rubajrafal04@gmail.com>
2023-09-16 16:37:33 +0200

    RoomPage grid layout

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -5,8 +5,17 @@ import "../styles/Room.scss";
 
 export default function RoomPage() {
   return (
-    <div className="roomWrapper">
+    <div className="roomGrid">
       <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+
+      <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+      <Camera username="GMBLR" score={420} />
+
       <Lobby roomCode="X1Y4Z3" />
     </div>
   );
