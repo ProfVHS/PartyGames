20ce9439dbcfa32b4b11316a1a1b3513c9478d84
Rafał Rubaj <rubajrafal04@gmail.com>
2023-11-15 15:50:09 +0100

    flippable cards

---
+++ client/src/components/Cards/index.tsx
@@ -0,0 +1,45 @@
+import "./style.scss";
+
+import Card from "./Card";
+import { useState } from "react";
+
+interface CardObject {
+  isPositive: boolean;
+  score: number;
+}
+
+function Cards() {
+  const cards: CardObject[] = [
+    { isPositive: true, score: 50 },
+    { isPositive: true, score: 50 },
+    { isPositive: false, score: 50 },
+    { isPositive: false, score: 50 },
+    { isPositive: true, score: 50 },
+
+    { isPositive: true, score: 50 },
+    { isPositive: true, score: 50 },
+    { isPositive: true, score: 50 },
+    { isPositive: false, score: 50 },
+  ];
+
+  const [flipped, setFlipped] = useState<boolean>(false);
+
+  return (
+    <div className="cards">
+      <span className="cards__title">Cards</span>
+      <div className="cardsWrapper" onClick={() => setFlipped(true)}>
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            isPositive={card.isPositive}
+            flip={flipped}
+            score={card.score}
+            index={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
