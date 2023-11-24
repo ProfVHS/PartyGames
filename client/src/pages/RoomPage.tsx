b849034f3984b090670c11379e25b786d6321650
Rafał Rubaj <rubajrafal04@gmail.com>
2023-11-24 11:07:08 +0100

    click the bomb responsivness, change camera font size to smaller and change max height for showing cameras

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -82,7 +82,7 @@ export default function RoomPage({ socket }: RoomPageProps) {
     <>
       <div className="roomGrid">
         {windowSizeX > 600 &&
-          windowSizeY > 600 &&
+          windowSizeY > 700 &&
           users &&
           users.map((user) => {
             return (
