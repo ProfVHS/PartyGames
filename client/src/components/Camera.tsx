356389dcc98cb55846df4541b8e1a752127a4af5
Rafał Rubaj <rubajrafal04@gmail.com>
2023-11-28 10:14:11 +0100

    joining animation using framer motion

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -1,18 +1,22 @@
 import "../styles/Camera.scss";
+import { motion } from "framer-motion";
 
 interface CameraProps {
   username: string;
   score: number;
 }
 
-function Camera({ username, score}: CameraProps) {
-  
+function Camera({ username, score }: CameraProps) {
   return (
-    <div className="camera">
+    <motion.div
+      className="camera"
+      animate={{ scale: [0.0, 1.0] }}
+      transition={{ duration: 1, type: "spring" }}
+    >
       <span className="camera__username">{username}</span>
       <video className="camera__video" autoPlay={true} />
       <span className="camera__score">Score: {score}</span>
-    </div>
+    </motion.div>
   );
 }
 
