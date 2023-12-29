1075b8a61c763b54b9d7afbea33726d88148335a
Rafal Rubaj <rubajrafal04@gmail.com>
2023-12-29 01:10:39 +0100

    changed css class names

--- client/src/components/Battleships/Board.tsx
+++ client/src/components/Battleships/Board.tsx
@@ -1,25 +1,21 @@
-
-
 export default function Board() {
+  const Rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const Columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-    const Rows = [1,2,3,4,5,6,7,8,9,10];
-    const Columns = ["A","B","C","D","E","F","G","H","I","J"];
-
-    return (
+  return (
     <>
-        <div className="board">
-            {Rows.map((row) => {
-                return (
-                    Columns.map((column) => {
-                        return (
-                            <div className="boardSquare">
-                                {column}{row}
-                            </div>
-                        )
-                    })
-                )
-            })}
-        </div>
+      <div className="battleships__board">
+        {Rows.map((row) => {
+          return Columns.map((column) => {
+            return (
+              <div className="battleships__boardSquare">
+                {column}
+                {row}
+              </div>
+            );
+          });
+        })}
+      </div>
     </>
-    )
+  );
 }
