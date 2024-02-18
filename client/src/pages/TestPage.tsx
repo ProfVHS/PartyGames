8ad42fcb9e9f183d41824b9e2e277b5cc72cff3f
Rafal Rubaj <rubajrafal04@gmail.com>
2024-02-18 23:51:39 +0100

    leaderboard flip animation - top 3 now have color background

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,7 +1,34 @@
-import { Buddies } from "../components/Buddies/Buddies";
-import { Question } from "../components/Buddies/Question";
-import { TrickyDiamonds } from "../components/TrickyDiamonds/TrickyDiamonds";
+import { useState } from "react";
+import Leaderboard from "../components/Leaderboard";
+
+const oldUsersLeaderBoard: { username: string; score: number }[] = [
+  { username: "user1", score: 100 },
+  { username: "user2", score: 200 },
+  { username: "user3", score: 300 },
+  { username: "user4", score: 400 },
+  { username: "user5", score: 500 },
+  { username: "user6", score: 600 },
+  { username: "user7", score: 700 },
+  { username: "user8", score: 800 },
+];
+
+const newUsersLeaderBoard: { username: string; score: number }[] = [
+  { username: "user1", score: 1000 },
+  { username: "user2", score: 200 },
+  { username: "user3", score: 300 },
+  { username: "user4", score: 400 },
+  { username: "user5", score: 500 },
+  { username: "user6", score: 600 },
+  { username: "user7", score: 700 },
+  { username: "user8", score: 800 },
+];
 
 export default function TestPage() {
-  return <></>;
+  const [test, setTest] = useState(true);
+  return (
+    <>
+      {test && <Leaderboard oldUsers={oldUsersLeaderBoard} newUsers={newUsersLeaderBoard} />}
+      <button onClick={() => setTest(!test)}>Test</button>
+    </>
+  );
 }
