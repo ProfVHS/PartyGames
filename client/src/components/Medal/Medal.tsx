b1addc82d5da0bd5bbca5131d381bdf65bf7b470
Rafał Rubaj <rubajrafal04@gmail.com>
2024-03-05 08:55:15 +0100

    medals animation

--- client/src/components/Medal/Medal.tsx
+++ client/src/components/Medal/Medal.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./style.scss";
 import { MedalSvg } from "./MedalSvg";
 import { LoseLineSvg, SkullSvg, TouchSvg } from "./Icons";
+import { useAnimate } from "framer-motion";
 
 interface MedalProps {
+  id: number;
   username: string;
   points: number;
   award: "ctbCLICK" | "firstDeath" | "mostLosedPoints";
 }
 
-export const Medal = ({ username, points, award }: MedalProps) => {
+export const Medal = ({ id, username, points, award }: MedalProps) => {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(".medal__user", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring" });
+    animate(".medal__svg", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring", delay: id + 1 });
+    animate(".medal__info", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring", delay: id + 1 });
+  }, []);
   return (
-    <div className="medal">
+    <div className="medal" ref={scope}>
       <div className="medal__user">{username}</div>
       <div className="medal__svg">
         <MedalSvg />
--- client/src/pages/EndGame/EndgamePage.tsx
+++ client/src/pages/EndGame/EndgamePage.tsx
@@ -1,5 +1,7 @@
+import { motion, useAnimate } from "framer-motion";
 import { Medal } from "../../components/Medal/Medal";
 import "./style.scss";
+import { useEffect } from "react";
 
 export default function EndgamePage() {
   return (
@@ -9,9 +11,9 @@ export default function EndgamePage() {
         <div className="endgame__partygames">Party Games</div>
       </div>
       <div className="endgame__content">
-        <Medal username="Ultra Mango Guy" points={150} award="ctbCLICK" />
-        <Medal username="Ultra Mango Guy" points={150} award="firstDeath" />
-        <Medal username="Ultra Mango Guy" points={150} award="mostLosedPoints" />
+        <Medal id={0} username="Ultra Mango Guy" points={150} award="ctbCLICK" />
+        <Medal id={1} username="Ultra Mango Guy" points={150} award="firstDeath" />
+        <Medal id={2} username="Ultra Mango Guy" points={150} award="mostLosedPoints" />
       </div>
     </div>
   );
