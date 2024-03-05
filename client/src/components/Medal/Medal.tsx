537f68f03336d40048d7215ae533586400dbcc29
Rafał Rubaj <rubajrafal04@gmail.com>
2024-03-05 09:12:22 +0100

    medal exit animation

--- client/src/components/Medal/Medal.tsx
+++ client/src/components/Medal/Medal.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import "./style.scss";
 import { MedalSvg } from "./MedalSvg";
 import { LoseLineSvg, SkullSvg, TouchSvg } from "./Icons";
-import { useAnimate } from "framer-motion";
+import { useAnimate, usePresence } from "framer-motion";
 
 interface MedalProps {
   id: number;
@@ -14,12 +14,28 @@ interface MedalProps {
 
 export const Medal = ({ id, username, points, award }: MedalProps) => {
   const [scope, animate] = useAnimate();
+  const [isPresence, safeToRemove] = usePresence();
+
+  useEffect(() => {
+    if (!isPresence) {
+      const exitAnimation = async () => {
+        await animate(
+          scope.current,
+          { opacity: [1, 0.8, 0], x: 1000 },
+          { duration: 1, type: "spring", delay: id * 0.25 }
+        );
+        safeToRemove();
+      };
+      exitAnimation();
+    }
+  }, [isPresence]);
 
   useEffect(() => {
     animate(".medal__user", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring" });
     animate(".medal__svg", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring", delay: id + 1 });
     animate(".medal__info", { opacity: [0, 1], y: [300, 0] }, { duration: 1, type: "spring", delay: id + 1 });
   }, []);
+
   return (
     <div className="medal" ref={scope}>
       <div className="medal__user">{username}</div>
--- client/src/pages/EndGame/EndgamePage.tsx
+++ client/src/pages/EndGame/EndgamePage.tsx
@@ -1,9 +1,17 @@
-import { motion, useAnimate } from "framer-motion";
+import { AnimatePresence, motion, useAnimate } from "framer-motion";
 import { Medal } from "../../components/Medal/Medal";
 import "./style.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function EndgamePage() {
+  const [showMedals, setShowMedals] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowMedals(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="endgame">
       <div className="endgame__header">
@@ -11,9 +19,15 @@ export default function EndgamePage() {
         <div className="endgame__partygames">Party Games</div>
       </div>
       <div className="endgame__content">
-        <Medal id={0} username="Ultra Mango Guy" points={150} award="ctbCLICK" />
-        <Medal id={1} username="Ultra Mango Guy" points={150} award="firstDeath" />
-        <Medal id={2} username="Ultra Mango Guy" points={150} award="mostLosedPoints" />
+        <AnimatePresence>
+          {showMedals ? <Medal id={0} username="Ultra Mango Guy" points={150} award="ctbCLICK" /> : null}
+        </AnimatePresence>
+        <AnimatePresence>
+          {showMedals ? <Medal id={1} username="Ultra Mango Guy" points={150} award="firstDeath" /> : null}
+        </AnimatePresence>
+        <AnimatePresence>
+          {showMedals ? <Medal id={2} username="Ultra Mango Guy" points={150} award="mostLosedPoints" /> : null}
+        </AnimatePresence>
       </div>
     </div>
   );
