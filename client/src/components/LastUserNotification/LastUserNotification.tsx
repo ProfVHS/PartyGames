2e75df34562ed2ce770118b60b1bc1881b80257e
Rafał <rubajrafal04@gmail.com>
2024-04-15 21:04:17 +0200

    last user notification styles

--- client/src/components/LastUserNotification/LastUserNotification.tsx
+++ client/src/components/LastUserNotification/LastUserNotification.tsx
@@ -3,33 +3,33 @@ import "./LastUserNotification.scss";
 import { socket } from "../../socket";
 
 import { usePresence } from "framer-motion";
+import { Hourglass } from "../Hourglass";
 
-interface LastUserNotificationProps{
-    onExit?: () => void;
+interface LastUserNotificationProps {
+  onExit?: () => void;
 }
 
-export default function LastUserNotification({onExit}: LastUserNotificationProps) {
-    const [isPresence, safeToRemove] = usePresence();
+export default function LastUserNotification({ onExit }: LastUserNotificationProps) {
+  const [isPresence, safeToRemove] = usePresence();
 
-    const handleLeaveRoom = () => {
-        window.location.href = "/";
-    };
+  const handleLeaveRoom = () => {
+    window.location.href = "/";
+  };
 
-    useEffect(() => {
-        if(isPresence){
+  useEffect(() => {
+    if (isPresence) {
+    } else {
+      onExit && onExit();
+    }
+  }, [isPresence]);
 
-        }
-        else{
-            onExit && onExit();
-        }
-    }, [isPresence])
-
-    return (
+  return (
     <>
-        <div id="last-user-dialog" className="last-user-notification">
-            <h1>Waiting for other players...</h1>
-            <button onClick={handleLeaveRoom}>Leave</button>
-        </div>
+      <div id="last-user-dialog" className="last-user-notification">
+        <h1>Waiting for other players...</h1>
+        <Hourglass />
+        <button onClick={handleLeaveRoom}>Leave</button>
+      </div>
     </>
-  )
+  );
 }
