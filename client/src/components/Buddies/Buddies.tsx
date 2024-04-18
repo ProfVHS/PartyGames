c9227fb75c5316dba2c0f7d8d03f68d82ee3dde3
Oliwier <worekit@gmail.com>
2024-04-18 15:28:51 +0200

    fix logic buddies

--- client/src/components/Buddies/Buddies.tsx
+++ client/src/components/Buddies/Buddies.tsx
@@ -11,6 +11,7 @@ import { Hourglass } from "../Hourglass";
 import { useAnimate, usePresence, motion } from "framer-motion";
 import { on } from "events";
 import { BestAnswer } from "./BestAnswer";
+import { ProgressBar } from "../ProgressBar";
 
 interface BuddiesProps {
   roomCode: string;
@@ -18,15 +19,34 @@ interface BuddiesProps {
   onExit?: () => void;
 }
 
+type GameStatus =
+  | "ANSWER"
+  | "QUESTION"
+  | "WAITINGANSWER"
+  | "WAITINGQUESTION"
+  | "SELECTANSWER"
+  | "BESTANSWER";
+
+const bestAnswerTimeMs = 5000;
+
+let timeInterval: NodeJS.Timeout;
+
 export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
-  const [allUsersWrittenQuestion, setAllUsersWrittenQuestion] = useState<number>(0);
-  const [allUsersWrittenAnswer, setAllUsersWrittenAnswer] = useState<number>(0);
   const [writtenQuestion, setWrittenQuestion] = useState<boolean>(false);
   const [writtenAnswer, setWrittenAnswer] = useState<boolean>(false);
-  const [question, setQuestion] = useState<QuestionType>({ author: "", question: "" });
+  const [question, setQuestion] = useState<QuestionType>({
+    author: "",
+    question: "",
+  });
+  const [bestAnswer, setBestAnswer] = useState<{
+    user: string;
+    answer: string;
+  }>({ user: "", answer: "" });
 
-  const [bestAnswer, setBestAnswer] = useState<{user: string, answer: string}>({user: "", answer: ""});
-  const [endGame, setEndGame] = useState<boolean>(false);
+  const [gameStatus, setGameStatus] = useState<GameStatus>("QUESTION");
+  const [gameOver, setGameOver] = useState<boolean>(false);
+
+  const [time, setTime] = useState<"STOP" | number>("STOP");
 
   const [scope, animate] = useAnimate();
   const [isPresence, safeToRemove] = usePresence();
@@ -36,9 +56,21 @@ export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
   useEffect(() => {
     if (isPresence) {
       const showUpElements = async () => {
-        await animate(".buddies__header", { scale: [0, 1], opacity: [0, 1] }, { duration: 0.5, type: "spring" });
-        animate(".buddies__button", { scale: [0, 1], opacity: [0, 1] }, { duration: 0.8, type: "spring" });
-        await animate(".buddies__input", { scale: [0, 1], opacity: [0, 1] }, { duration: 0.8, type: "spring" });
+        await animate(
+          ".buddies__header",
+          { scale: [0, 1], opacity: [0, 1] },
+          { duration: 0.5, type: "spring" }
+        );
+        animate(
+          ".buddies__button",
+          { scale: [0, 1], opacity: [0, 1] },
+          { duration: 0.8, type: "spring" }
+        );
+        await animate(
+          ".buddies__input",
+          { scale: [0, 1], opacity: [0, 1] },
+          { duration: 0.8, type: "spring" }
+        );
       };
       const enterAnimation = async () => {
         await animate(scope.current, { scaleY: [0, 1] }, { duration: 0.5 });
@@ -47,7 +79,11 @@ export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
       enterAnimation();
     } else {
       const exitAnimation = async () => {
-        await animate(scope.current, { scale: [1, 0] }, { duration: 0.5, type: "spring" });
+        await animate(
+          scope.current,
+          { scale: [1, 0] },
+          { duration: 0.5, type: "spring" }
+        );
         safeToRemove();
         onExit && onExit();
       };
@@ -64,34 +100,50 @@ export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
   };
 
   useEffect(() => {
-    const isEveryUserHasQuestion = (data: number) => {
-      setAllUsersWrittenQuestion(data);
+    const isEveryUserHasQuestion = (data: GameStatus) => {
+      setGameStatus(data);
+
+      if (data === "ANSWER") {
+        const host = users.find((user) => user.id === socket.id)?.is_host;
+
+        if (!host) return;
+
+        socket.emit("getQuestionsBuddies", roomCode);
+      }
     };
 
-    const isEveryUserHasAnswer = (data: number) => {
-      const temp = data;
-      setAllUsersWrittenAnswer(temp);
+    const isEveryUserHasAnswer = (data: GameStatus) => {
+      setGameStatus(data);
+
+      if (data === "SELECTANSWER") {
+        const host = users.find((user) => user.id === socket.id)?.is_host;
+
+        if (!host) return;
+
+        socket.emit("getAnswersBuddies", roomCode);
+      }
     };
 
     const receiveQuestion = (question: string, user: string) => {
       setQuestion({ author: user, question: question });
     };
 
-    const receiveTheBestAnswer = (data: {user: string, answer: string}) => {
+    const receiveTheBestAnswer = (
+      data: { user: string; answer: string },
+      status: GameStatus
+    ) => {
       setBestAnswer(data);
-
-      setTimeout(() => {
-        setBestAnswer({user: "", answer: ""});
-      }, 5000);
+      setGameStatus(status);
+      setTime(0);
     };
 
     const newRound = () => {
-      setAllUsersWrittenAnswer(0);
+      setGameStatus("ANSWER");
       setWrittenAnswer(false);
     };
 
-    const endGameFunction = () => {
-      setEndGame(true);
+    const gameOver = () => {
+      setGameOver(true);
     };
 
     socket.on("allQuestionsBuddies", isEveryUserHasQuestion);
@@ -104,37 +156,19 @@ export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
 
     socket.on("newRoundBuddies", newRound);
 
-    socket.on("endGameBuddies", endGameFunction);
+    socket.on("receiveGameOver", gameOver);
 
     return () => {
       socket.off("allQuestionsBuddies", isEveryUserHasQuestion);
       socket.off("allAnswersBuddies", isEveryUserHasAnswer);
       socket.off("receiveQuestionBuddies", receiveQuestion);
       socket.off("newRoundBuddies", newRound);
-      socket.off("endGameBuddies", endGameFunction);
     };
   }, [socket]);
 
   useEffect(() => {
-    const host = users.find((user) => user.id === socket.id)?.is_host;
-
-    const activeUsers = users.filter((user) => !user.is_disconnect);
-
-    if (allUsersWrittenQuestion === activeUsers.length) {
-      if (host) {
-        socket.emit("getQuestionsBuddies", roomCode);
-      }
-    }
-    if (allUsersWrittenAnswer === activeUsers.length - 1) {
-      if (host) {
-        socket.emit("getAnswersBuddies", roomCode);
-      }
-    }
-  }, [allUsersWrittenQuestion, allUsersWrittenAnswer]);
-
-  useEffect(() => {
     if (onceDone.current) return;
-    
+
     const host = users.find((user) => user.id === socket.id)?.is_host;
 
     if (host) socket.emit("initBestBuddiesAnswers", roomCode);
@@ -142,34 +176,92 @@ export function Buddies({ roomCode, users, onExit }: BuddiesProps) {
     onceDone.current = true;
   }, []);
 
+  /* =========== Timer =========== */
   useEffect(() => {
-    if(allUsersWrittenQuestion === users.filter((user) => !user.is_disconnect).length-1) {
-      setWrittenQuestion(true);
-    }
-  }, []);
+    if (time === "STOP") return;
+
+    const host = users.find((user) => user.id === socket.id)?.is_host;
+
+    timeInterval = setInterval(() => {
+      if (time <= bestAnswerTimeMs) {
+        const newTime = time + 10;
+        setTime(newTime);
+      } else {
+        clearInterval(timeInterval);
+        setBestAnswer({ user: "", answer: "" });
+        setTime("STOP");
+
+        if (!host) return;
+
+        if (gameOver) {
+          socket.emit("endGameBuddies", roomCode);
+        } else {
+          socket.emit("getQuestionsBuddies", roomCode);
+        }
+      }
+    }, 10);
+
+    return () => clearInterval(timeInterval);
+  }, [time]);
 
   return (
     <div className="buddies" ref={scope}>
-      {endGame 
-      ? (<></>) 
-      : !writtenQuestion 
-        ? <Question roomCode={roomCode} users={users} onClick={isQuestionWritten} />
-        : allUsersWrittenQuestion !== users.filter((user) => !user.is_disconnect).length 
-          ? (<>
-              <h3 className="buddies__waiting">Waiting for all players to ask the questions</h3>
-              <Hourglass />
-            </>) 
-          : writtenAnswer || socket.id === question.author 
-            ? allUsersWrittenAnswer !== users.filter((user) => !user.is_disconnect).length - 1
-              ? (<>
-                  <h3 className="buddies__waiting">Waiting for all players to answer</h3>
-                  <Hourglass />
-                </>) 
-              : bestAnswer.answer !== "" 
-                ? <BestAnswer bestAnswer={bestAnswer} />
-                : <AnswersSelect roomCode={roomCode} users={users} question={question} />
-            : <Answer roomCode={roomCode} users={users} onClick={isAnswerWritten} question={question} />
-      }
+      {gameStatus === "QUESTION" && !writtenQuestion && (
+        <Question
+          roomCode={roomCode}
+          users={users}
+          onClick={isQuestionWritten}
+        />
+      )}
+      {gameStatus === "QUESTION" && writtenQuestion && (
+        <>
+          <h3 className="buddies__waiting">
+            Waiting for all players to ask the questions
+          </h3>
+          <Hourglass />
+        </>
+      )}
+      {gameStatus === "ANSWER" &&
+        !writtenAnswer &&
+        socket.id !== question.author && (
+          <Answer
+            roomCode={roomCode}
+            users={users}
+            onClick={isAnswerWritten}
+            question={question}
+          />
+        )}
+      {gameStatus === "ANSWER" &&
+        writtenAnswer &&
+        socket.id !== question.author && (
+          <>
+            <h3 className="buddies__waiting">
+              Waiting for all players to answer
+            </h3>
+            <Hourglass />
+          </>
+        )}
+      {gameStatus === "ANSWER" && socket.id === question.author && (
+        <>
+          <h3 className="buddies__waiting">
+            Waiting for players to answer to your question
+          </h3>
+          <Hourglass />
+        </>
+      )}
+      {gameStatus === "SELECTANSWER" && (
+        <AnswersSelect roomCode={roomCode} users={users} question={question} />
+      )}
+      {gameStatus === "BESTANSWER" && (
+        <>
+          <BestAnswer bestAnswer={bestAnswer} />
+          <ProgressBar
+            width="75%"
+            max={bestAnswerTimeMs}
+            progress={typeof time === "number" ? time : 0}
+          />
+        </>
+      )}
     </div>
   );
 }
