9f7045e24825f8e227443bd06149403b70ed792d
Rafał <rubajrafal04@gmail.com>
2024-04-19 18:31:19 +0200

    count up score animation

--- client/src/components/Camera/Camera.tsx
+++ client/src/components/Camera/Camera.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "./style.scss";
+
 import { motion } from "framer-motion";
 import { NoSignalIcon } from "./IconsForCamera";
+import { useCountUp } from "react-countup";
 
 interface CameraProps {
   username: string;
@@ -9,39 +11,28 @@ interface CameraProps {
   isDisconnected: boolean;
 }
 
-const scoreAnimationDuration = 0.5;
-
 function Camera({ username, score, isDisconnected }: CameraProps) {
-  const [userScore, setUserScore] = useState<number>(0);
+  const countUpRef = useRef(null);
 
-  const interval = useRef<NodeJS.Timeout>();
+  const { update } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    end: score,
+    duration: 1,
+  });
 
   useEffect(() => {
-    return () => clearInterval(interval.current);
-  }, []);
-
-  useEffect(() => {
-    if (score > userScore) {
-      interval.current = setInterval(() => {
-        setUserScore((prevScore) => prevScore + 1);
-      }, scoreAnimationDuration / score);
-    } else {
-      interval.current = setInterval(() => {
-        setUserScore((prevScore) => prevScore - 1);
-      }, scoreAnimationDuration / score);
-    }
+    update(score);
   }, [score]);
-
-  useEffect(() => {
-    if (userScore === score) clearInterval(interval.current);
-  }, [userScore]);
 
   return (
     <motion.div className={`camera ${isDisconnected ? "disconnected" : ""}`} initial={{ scale: 0.0 }} animate={{ scale: [0.0, 1.0] }} transition={{ duration: 1, type: "spring" }}>
       <span className="camera__username">{username}</span>
       <video className="camera__video" autoPlay={true} />
       {isDisconnected ? <NoSignalIcon className="camera__icon" /> : null}
-      <span className="camera__score">Score: {userScore}</span>
+      <span className="camera__score">
+        Score: <span ref={countUpRef} />
+      </span>
     </motion.div>
   );
 }
